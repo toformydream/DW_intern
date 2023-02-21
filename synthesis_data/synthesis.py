358f10e892bec283939b37026bf8a7720739413b
허대원 <108973791+toformydream@users.noreply.github.com>
2023-02-21 16:33:05 +0900

    Update synthesis.py

--- synthesis_data/synthesis.py
+++ synthesis_data/synthesis.py
@@ -9,7 +9,6 @@ import json
 4. 이미지 위치 선택
 5. 합성
 6. 2만번 반복 
-:return:
 """
 
 
@@ -24,13 +23,11 @@ def main():
     name = ['knife', 'gun', 'laser_pointer', 'battery']
     for i in range(0, 4):
         append_categories_json(synthesis_data, name, i)
-    while synthesis_image_number <3:
-        items, l_items, img_number, item_seg = get_ramdom_img(item_seg)  # item 이미지와 이미지 넘버를 담은리스트
+    while synthesis_image_number <10000:
+        items, l_items, img_number, item_seg, category_id, iscrowd, color, unitID, registNum, number1, number2, weight = get_ramdom_img(item_seg)  # item 이미지와 이미지 넘버를 담은리스트
         high_path, low_path = get_background_image() # 경로임
-        high_background = cv2.imread(high_path)
-        low_background = cv2.imread(low_path)
-        high_background = normalize(high_background)
-        low_background = normalize(low_background)
+        high_background = normalize(high_path)
+        low_background = normalize(low_path)
         h_b_h, h_b_w = high_background.shape[:2]
         l_b_h, l_b_w = low_background.shape[:2]
         append_files_json(synthesis_data, synthesis_image_number, h_b_w, h_b_h)  # TODO
@@ -65,13 +62,13 @@ def main():
             for c_x in range(h_x, h_x + l_w):
                 for c_y in range(h_y, h_y + l_h):
                     low_background[c_y][c_x] = low_background[c_y][c_x] * normalized_low_image[c_y -h_y][c_x - h_x]
-            append_items_json(synthesis_data,synthesis_image_number,h_x, h_y, l_w, l_h, segmentation , json_item_number)
+            append_items_json(synthesis_data,synthesis_image_number,h_x, h_y, l_w, l_h, segmentation , json_item_number,category_id, iscrowd, color, unitID, registNum, number1, number2, weight)
             json_item_number = json_item_number+1
-        cv2.imwrite(f"D:/wp/DW_intern/dist/{synthesis_image_number}.png", high_background*256)
-        # cv2.imwrite(f"D:/wp/DW_intern/synthesis_low_data/{synthesis_image_number}.png", low_background*256)
+        cv2.imwrite(f"C:/DW_intern-main/high_synthesis/{synthesis_image_number}.png", high_background*256)
+        cv2.imwrite(f"C:/DW_intern-main/low_synthesis/{synthesis_image_number}.png", low_background*256)
         print(synthesis_image_number)
         synthesis_image_number += 1
-    with open('D:/wp/DW_intern/json/verification_data.json', 'w', encoding='utf-8') as file:
+    with open('C:/DW_intern-main/json/synthesis.json', 'w', encoding='utf-8') as file:
         json.dump(synthesis_data, file, indent=4)
 
 
@@ -94,7 +91,7 @@ def append_files_json(synthesis_data, synthesis_image_number, b_w, b_h):
     synthesis_data['images'].append({
         'id': synthesis_image_number,
         'dataset_id': '',
-        'path': f"D:/wp/DW_intern/synthesis_high_data/{synthesis_image_number}.png",
+        'path': f"C:/DW_intern-main/high_synthesis/{synthesis_image_number}.png",
         'file_name': f"{synthesis_image_number}.png",
         'width': b_w,
         'height': b_h
@@ -105,28 +102,28 @@ def append_categories_json(synthesis_data, name, i):
     synthesis_data['categories'].append({
         'id': i + 1,
         'name': name[i],
-        'supercategory': '',
-        'color': '',
+        'supercategory': 'item',
+        'color': '040439',
         'metatdata': ''
     })
 
 
 def append_items_json(synthesis_data,synthesis_image_number, x, y, w, h,
-                      seg,json_item_number):
+                      seg,json_item_number,category_id, iscrowd, color, unitID, registNum, number1, number2, weight):
     synthesis_data['annotations'].append({
         'id': json_item_number,
         'image_id' : synthesis_image_number,
-        'category_id': '',
+        'category_id': category_id,
         'bbox': [x, y, x + w, y + h],
         'segmentation': seg,
         'area': w * h,
-        'iscrowd': '',
-        'color': '',
-        'unitID': '',
-        'registNum': '',
-        'number1': '',
-        'number2': '',
-        'weight': ''
+        'iscrowd': iscrowd,
+        'color': color,
+        'unitID': unitID,
+        'registNum': registNum,
+        'number1': number1,
+        'number2': number2,
+        'weight': weight
     })
     return synthesis_data
 
@@ -144,36 +141,48 @@ def normalize(image):
 
 
 def get_background_image():
-    count = random.randrange(1, 5)
-    high_img = f"D:/wp/DW_intern/background/high_{count}.png"
-    low_img = f"D:/wp/DW_intern/background/low_{count}.png"
-    return high_img, low_img
+    count = random.randrange(1,5)
+    high_img = cv2.imread(f"C:/DW_intern-main/background/high_{count}.png")
+    low_img = cv2.imread(f"C:/DW_intern-main/background/low_{count}.png")
+    resize_high = cv2.resize(high_img, dsize=(700,700), interpolation = cv2.INTER_AREA)
+    resize_low = cv2.resize(low_img, dsize=(700, 700), interpolation=cv2.INTER_AREA)
+    return resize_high,resize_low
 
 
 def get_random_coordinate(img, b_h, b_w):
     h, w = img.shape[:2]
-    new_x = round(random.uniform(0, b_w - w-10))
-    new_y = round(random.uniform(0, b_h - h-10))
+    new_x = round(random.uniform(0, b_w - w-100))
+    new_y = round(random.uniform(0, b_h - h-100))
     return new_x, new_y, h, w,
+
 
 def get_ramdom_img(item_seg):
     json_data = ['gun_json', 'battery_json', 'knife_json', 'laser_json']
-    data_files = ['ground_truth_gun_data_file', 'ground_truth_battery_data_file', 'ground_truth_knife_data_file',
-                  'ground_truth_laser_point_data_file']
+    json_path = ['gun','battery','knife','laser']
+    data_files = ['gun_data_file', 'battery_data_file', 'knife_data_file',
+                  'laser_data_file']
     for i in range(4):
-        json_data[i] = load_json(f"D:/wp/DW_intern/json/{data_files[i]}.json")
+        json_data[i] = load_json(f"C:/DW_intern-main/json/{json_path[i]}.json")
     h_items = []
     l_items = []
     file_numbers = []
     for i in range(0, 10):
         file = random.randrange(0, 4)
         files = data_files[file]
-        file_number = random.randrange(0, 10000, 2)
+        file_number = random.randrange(0, 9900, 2)
+        category_id = json_data[file]['annotations'][file_number]['category_id']
         item_seg[i] = json_data[file]['annotations'][file_number]['segmentation']
+        iscrowd = json_data[file]['annotations'][file_number]['iscrowd']
+        color = json_data[file]['annotations'][file_number]['color']
+        unitID = json_data[file]['annotations'][file_number]['unitID']
+        registNum = json_data[file]['annotations'][file_number]['registNum']
+        number1 = json_data[file]['annotations'][file_number]['number1']
+        number2 = json_data[file]['annotations'][file_number]['number2']
+        weight = json_data[file]['annotations'][file_number]['weight']
         file_numbers.append(file_number)
-        h_items.append(f"D:/wp/DW_intern/{files}/{file_number}.png")
-        l_items.append(f"D:/wp/DW_intern/{files}/{file_number + 1}.png")
-    return h_items, l_items, file_numbers, item_seg
+        h_items.append(f"C:/DW_intern-main/{files}/{file_number}.png")
+        l_items.append(f"C:/DW_intern-main/{files}/{file_number + 1}.png")
+    return h_items, l_items, file_numbers, item_seg, category_id, iscrowd, color, unitID, registNum, number1, number2, weight
 
 
 if __name__ == '__main__':
